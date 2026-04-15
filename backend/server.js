769a977ad2931aef0c5b1cf4b5b7fb0108b3fb86
navneetchinchole04 <navneetchinchole04@gmail.com>
2026-04-15 23:49:13 +0530

    fixed port for deployment

--- backend/server.js
+++ backend/server.js
@@ -18,3 +18,9 @@ app.use("/api", searchRoutes);
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
